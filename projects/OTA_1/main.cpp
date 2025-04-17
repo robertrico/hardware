275f8f5b99621d6a298ddb04aa363f91f4c3994c
Robert Rico <robert.m.rico@gmail.com>
2025-04-17 14:21:20 -0700

    Adds status LEDs for Wi-Fi connection
    Adds status LEDs to indicate Wi-Fi connection status.
    Green indicates successful connection to the internet.
    Yellow indicates Wi-Fi connected, but no internet access.
    Red indicates Wi-Fi connection failure.

--- projects/OTA_1/main.cpp
+++ projects/OTA_1/main.cpp
@@ -6,9 +6,12 @@
 #include "pico/cyw43_arch.h"
 #include "wifi_manager.hpp"
 
+#define LED_GRN 14
+#define LED_RED 15
+#define LED_YLW 16
+
 int main() {
     stdio_init_all();
-
     printf("Initializing Wi-Fi Connection (CPP)...\n");
     if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
         printf("CYW43 init failed!\n");
@@ -17,14 +20,25 @@ int main() {
 
     WiFiManager wifi;
 
+    gpio_init(LED_GRN);
+    gpio_init(LED_RED);
+    gpio_init(LED_YLW);
+
+    gpio_set_dir(LED_GRN, GPIO_OUT);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+    gpio_set_dir(LED_YLW, GPIO_OUT);
+
     if (wifi.connect()) {
         if (wifi.testConnectivity()) {
             printf("Internet is OK!\n");
+            gpio_put(LED_GRN, 1);
         } else {
             printf("No Internet!\n");
+            gpio_put(LED_YLW, 1);
         }
     } else {
         printf("Wi-Fi Failed!\n");
+        gpio_put(LED_RED, 1);
     }
 
     return 0;
